3ba8a5c85ca79efa8bbf4aedbe9c60a335207bbc
YonVanom <yonvanom@gmail.com>
2026-02-19 20:52:05 -0500

    Fixed particle_filter range_libc pywrapper setup.py

--- f1tenth/particle_filter/range_libc/pywrapper/setup.py
+++ f1tenth/particle_filter/range_libc/pywrapper/setup.py
@@ -95,8 +95,10 @@ def locate_cuda():
 
 
 # compiler_flags = ["-w","-std=c++11", "-march=native", "-ffast-math", "-fno-math-errno"]
-compiler_flags = ["-w","-std=c++11", "-march=native", "-ffast-math", "-fno-math-errno", "-O3"]
-nvcc_flags = ['-arch=sm_62', '--ptxas-options=-v', '-c', '--compiler-options', "'-fPIC'", "-w","-std=c++11"]
+#compiler_flags = ["-w","-std=c++11", "-march=native", "-ffast-math", "-fno-math-errno", "-O3"]
+compiler_flags = ["-w","-std=c++11", "-ffast-math", "-fno-math-errno", "-O3"]
+#nvcc_flags = ['-arch=sm_68', '--ptxas-options=-v', '-c', '--compiler-options', "'-fPIC'", "-w","-std=c++11"]
+nvcc_flags = ['-arch=sm_87', '--ptxas-options=-v', '-c', '--compiler-options', "'-fPIC'", "-w","-std=c++11"]
 include_dirs = ["../", numpy_include]
 depends = ["../includes/*.h"]
 sources = ["RangeLibc.pyx","../vendor/lodepng/lodepng.cpp"]
@@ -108,10 +110,10 @@ if use_cuda:
     compiler_flags.append("-DUSE_CUDA=1");        nvcc_flags.append("-DUSE_CUDA=1")
     compiler_flags.append("-DCHUNK_SIZE="+CHUNK_SIZE); nvcc_flags.append("-DCHUNK_SIZE="+CHUNK_SIZE)
     compiler_flags.append("-DNUM_THREADS="+NUM_THREADS);   nvcc_flags.append("-DNUM_THREADS="+NUM_THREADS)
-    compiler_flags.append("-DCMAKE_C_COMPILER=$(which gcc-9)")
-    compiler_flags.append("-DCMAKE_CXX_COMPILER=$(which g++-9)")
-    nvcc_flags.append("-DCMAKE_C_COMPILER=$(which gcc-9)")
-    nvcc_flags.append("-DCMAKE_CXX_COMPILER=$(which g++-9)")
+    #compiler_flags.append("-DCMAKE_C_COMPILER=$(which gcc-9)")
+    #compiler_flags.append("-DCMAKE_CXX_COMPILER=$(which g++-9)")
+    #nvcc_flags.append("-DCMAKE_C_COMPILER=$(which gcc-9)")
+    #nvcc_flags.append("-DCMAKE_CXX_COMPILER=$(which g++-9)")
 
     CUDA = locate_cuda()
     include_dirs.append(CUDA['include'])
@@ -119,8 +121,8 @@ if use_cuda:
 
 if trace:
     compiler_flags.append("-D_MAKE_TRACE_MAP=1")
-    compiler_flags.append("-DCMAKE_C_COMPILER=$(which gcc-9)")
-    compiler_flags.append("-DCMAKE_CXX_COMPILER=$(which g++-9)")
+    #compiler_flags.append("-DCMAKE_C_COMPILER=$(which gcc-9)")
+    #compiler_flags.append("-DCMAKE_CXX_COMPILER=$(which g++-9)")
 
 
 ##################################################################
@@ -153,6 +155,7 @@ def customize_compiler_for_nvcc(self):
             # from the extra_compile_args in the Extension class
             postargs = extra_postargs['nvcc']
         else:
+            self.set_executable('compiler_so', default_compiler_so)
             postargs = extra_postargs['gcc']
         # postargs = extra_postargs#['gcc']
 
